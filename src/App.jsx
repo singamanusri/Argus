0d1870e701a1eda5dbfe8f7416cc44e5cdda7834
Your Name <your_email@example.com>
2026-08-01 10:32:43 +0530

    Add IP check function

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+
+import { useState } from "react";
+
+
+function getColor(severity) {
+  if (severity === "critical") return "red";
+  if (severity === "medium") return "orange";
+  if (severity === "low") return "green";
+  return "gray";
+}
+
+function App() {
+const [selectedAlert, setSelectedAlert] = useState(null);
+const [ipReport, setIpReport] = useState(null);
+  const alerts = [
+     {
+      id: "ALT-1001",
+      title: "Unusual login time detected",
+      severity: "critical",
+      ioc: "185.220.101.47",
+    },
+    {
+      id: "ALT-1002",
+      title: "Multiple failed login attempts",
+      severity: "medium",
+      ioc: "user: r.mehta",
+    },
+    {
+      id: "ALT-1003",
+      title: "New device connected to network",
+      severity: "low",
+      ioc: "AA:BB:CC:11:22:33",
+    },
+  ];
+async function checkIP(ip) {
+  const response = await fetch(
+    `https://api.abuseipdb.com/api/v2/check?ipAddress=${ip}`,
+    {
+      headers: {
+        Key: import.meta.env.VITE_ABUSEIPDB_KEY,
+        Accept: "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  setIpReport(data.data);
+}
+  return (
+    <div>
+      <h1>Argus</h1>
+      <p>A hundred eyes. Zero blind spots.</p>
+
+      <h2>Alerts</h2>
+      {alerts.map((alert) => (
+  <div
+    key={alert.id}
+    onClick={() => {
+      setSelectedAlert(alert);
+      setIpReport(null);
+      if (alert.iocType === "ip" || /^\d+\.\d+\.\d+\.\d+$/.test(alert.ioc)) {
+        checkIP(alert.ioc);
+      }
+    }}
+    style={{
+      border: "1px solid " + getColor(alert.severity),
+      padding: "10px",
+      margin: "10px 0",
+      cursor: "pointer",
+    }}
+  >
+    <p>{alert.title}</p>
+    <p style={{ color: getColor(alert.severity) }}>
+      Severity: {alert.severity}
+    </p>
+    <p>IOC: {alert.ioc}</p>
+  </div>
+))}
+          
+
+      {selectedAlert && (
+  <div style={{ marginTop: "20px", padding: "10px", border: "2px solid white" }}>
+    <h3>Alert Details</h3>
+    <p>ID: {selectedAlert.id}</p>
+    <p>Title: {selectedAlert.title}</p>
+    <p>Severity: {selectedAlert.severity}</p>
+    <p>IOC: {selectedAlert.ioc}</p>
+  </div>
+)}
+    </div>
+  );
+}
+
+export default App;
